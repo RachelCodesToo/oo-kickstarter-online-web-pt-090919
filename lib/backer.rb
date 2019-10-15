fcd3bbbe8403b0651635cadbf44833383b9f9cab
Anne Fausto <a.r.fausto@gmail.com>
2019-10-15 21:05:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,11 +2,11 @@ class Backer
   
   attr_reader :name 
   
-  
+  @@backed_projects = []
   
   def initialize(name)
     @name = name 
-    @backed_projects = []
+    #@backed_projects = []
   end 
   
 end 
